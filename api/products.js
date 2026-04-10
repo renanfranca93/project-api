0827093cfff05ec206457796d4d933fe0d59cd2e
Renan <renanfranca.93@gmail.com>
2026-04-10 07:10:08 -0300

    adicionado metodo post

--- api/products.js
+++ api/products.js
@@ -5,9 +5,19 @@ const products = [
 ];
 
 module.exports = (req, res) => {
-  if (req.method !== 'GET') {
-    res.setHeader('Allow', 'GET');
+  if (req.method === 'GET') {
+    return res.status(200).json(products);
+  }
+
+  if (req.method === 'POST') {
+    const newProduct =
+      typeof req.body === 'string' ? JSON.parse(req.body || '{}') : req.body;
+
+    return res.status(200).json([...products, newProduct]);
+  }
+
+  if (req.method !== 'GET' && req.method !== 'POST') {
+    res.setHeader('Allow', 'GET, POST');
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
-  return res.status(200).json(products);
 };
