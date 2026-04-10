41702ed7c9d5926176d25eca52796f85159aa908
Renan <renanfranca.93@gmail.com>
2026-04-09 22:42:41 -0300

    correção do module exports

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-MediaSourceHandle.exports = (req, res) => {
-  return res.status(200).json({ 
-    message: 'API Online' 
-});
-}
+module.exports = (req, res) => {
+  return res.status(200).json({
+    message: 'API Online',
+  });
+};
